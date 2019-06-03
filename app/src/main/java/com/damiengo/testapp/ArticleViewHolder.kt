aceaa170b0540d7c0f5b30f78fcc34ec31af4cf7
damiengo <d@geon.fr>
2019-06-03 22:16:42 +0200

    Added categories

--- app/src/main/java/com/damiengo/testapp/ArticleViewHolder.kt
+++ app/src/main/java/com/damiengo/testapp/ArticleViewHolder.kt
@@ -9,14 +9,34 @@ import com.prof.rssparser.Article
 class ArticleViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
     RecyclerView.ViewHolder(inflater.inflate(R.layout.article_item, parent, false)) {
     private var titleView: TextView? = null
+    private var categoryView: TextView? = null
 
+    private var articleTitle: String? = null
+    private var articleCategory: String? = null
 
     init {
         titleView = itemView.findViewById(R.id.article_title)
+        categoryView = itemView.findViewById(R.id.article_category)
     }
 
     fun bind(article: Article) {
-        titleView?.text = article.title
+        setArticleText(article)
+        titleView?.text = articleTitle
+        categoryView?.text = articleCategory
+    }
+
+    private fun setArticleText(article: Article) {
+        val title = article.title
+
+        val lastIdx = title?.substring(0, 30)!!.lastIndexOf(" - ")
+
+        articleCategory = ""
+        articleTitle = article.title
+
+        if(lastIdx != -1) {
+            articleCategory = title?.substring(0, lastIdx)
+            articleTitle    = title?.substring(lastIdx+3)
+        }
     }
 
 }
