70eb7d107304c3b3fa50cc5b8b4c686a701e923d
damiengo <d@geon.fr>
2019-06-05 21:54:22 +0200

    Article detail

--- app/src/main/java/com/damiengo/testapp/ArticleDetailActivity.kt
+++ app/src/main/java/com/damiengo/testapp/ArticleDetailActivity.kt
@@ -2,6 +2,8 @@ package com.damiengo.testapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.article_detail_activity.*
 import java.util.logging.Logger
 
 class ArticleDetailActivity : AppCompatActivity() {
@@ -11,6 +13,19 @@ class ArticleDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.article_detail_activity)
+
+        val title = intent.getStringExtra("title")
+        val description = intent.getStringExtra("description")
+        val image = intent.getStringExtra("image")
+        val pubDate = intent.getStringExtra("pubDate")
+
+        article_title.text = title
+        article_description.text = description
+        article_date.text = pubDate
+
+        Glide.with(this)
+             .load(image)
+             .into(article_image)
     }
 
 }
