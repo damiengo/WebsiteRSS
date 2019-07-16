27049a636bad53443ccab4ff77849f7c637dfb95
damiengo <d@geon.fr>
2019-07-16 22:08:10 +0200

    Glide preloading

--- app/src/main/java/com/damiengo/websiterss/util/MyAppGlideModule.kt
+++ app/src/main/java/com/damiengo/websiterss/util/MyAppGlideModule.kt
@@ -1,7 +1,16 @@
 package com.damiengo.websiterss.util
 
+import android.content.Context
+import android.util.Log
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.GlideBuilder
 
 @GlideModule
-class MyAppGlideModule : AppGlideModule()
+class MyAppGlideModule : AppGlideModule() {
+
+    /*override fun applyOptions(context: Context, builder: GlideBuilder) {
+        builder.setLogLevel(Log.VERBOSE)
+    }*/
+
+}
