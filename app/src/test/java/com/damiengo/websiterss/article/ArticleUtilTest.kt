5f44369b52274a39a6d3975e0317767c9f561608
damiengo <d@geon.fr>
2019-10-07 21:45:56 +0200

    Unit tests

--- app/src/test/java/com/damiengo/websiterss/article/ArticleUtilTest.kt
+++ app/src/test/java/com/damiengo/websiterss/article/ArticleUtilTest.kt
@@ -1,7 +1,6 @@
 package com.damiengo.websiterss.article
 
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
@@ -15,18 +14,43 @@ class ArticleUtilTest {
     }
 
     @Test
-    fun testSetTitleWithDashLessThanMaxSize() {
-        assertEquals("dashes", u.setTitle("Title with - dashes"))
+    fun genTitleWithDashLessThanMaxSize() {
+        assertEquals("dashes", u.genTitle("Title with - dashes"))
     }
 
     @Test
-    fun testSetTitleWithDashMoreThanMaxSize() {
-        assertEquals("30 characterz", u.setTitle("Title with dashes and a size of title larger than - 30 characters"))
+    fun genTitleWithDashesLessThanMaxSize() {
+        assertEquals("dash 3", u.genTitle("Title with - dash1 - dash 2 - dash 3"))
     }
 
     @Test
-    fun testSetTitleWithoutDashes() {
-        assertEquals("Title without dashes", u.setTitle("Title without dashes"))
+    fun genTitleWithDashesLessAndMoreThanMaxSize() {
+        assertEquals("dash 3 - dash 4", u.genTitle("Title with - dash1 - dash 2 - dash 3 - dash 4"))
+    }
+
+    @Test
+    fun genTitleWithDashMoreThanMaxSize() {
+        assertEquals("Title with dashes and a size of title larger than - 30 characters", u.genTitle("Title with dashes and a size of title larger than - 30 characters"))
+    }
+
+    @Test
+    fun genTitleWithoutDashes() {
+        assertEquals("Title without dashes", u.genTitle("Title without dashes"))
+    }
+
+    @Test
+    fun genCategoriesOneCategory() {
+        assertEquals(mutableListOf("Category 1"), u.genCategories("Category 1 - A title with only one category"))
+    }
+
+    @Test
+    fun genCategoriesMoreThanOneCategory() {
+        assertEquals(mutableListOf("Category 1", "Category 2"), u.genCategories("Category 1 - Category 2 - A title with several categories"))
+    }
+
+    @Test
+    fun genCategoriesNoCategory() {
+        assertEquals(mutableListOf<String>(), u.genCategories("A title with no category detected because to lon title - Title"))
     }
 
 }
