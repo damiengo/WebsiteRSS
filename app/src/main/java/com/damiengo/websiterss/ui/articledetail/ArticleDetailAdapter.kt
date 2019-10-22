c86e5660df7939ae78f461657574ac763a4eded0
damiengo <d@geon.fr>
2019-10-22 22:06:47 +0200

    Article Detail recycler view

--- app/src/main/java/com/damiengo/websiterss/ui/articledetail/ArticleDetailAdapter.kt
+++ app/src/main/java/com/damiengo/websiterss/ui/articledetail/ArticleDetailAdapter.kt
@@ -1,19 +1,52 @@
 package com.damiengo.websiterss.ui.articledetail
 
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.damiengo.websiterss.R
+import com.damiengo.websiterss.ui.articledetail.model.ParagraphModel
 
-class ArticleDetailAdapter: RecyclerView.Adapter<BaseViewHolder<*>>() {
+class ArticleDetailAdapter(private val context: Context): RecyclerView.Adapter<BaseViewHolder<*>>() {
+
+    private var adapterDataList: List<Any> = emptyList()
+
+    companion object {
+        private const val TYPE_PARAGRAPH = 0
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<*> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return when (viewType) {
+            TYPE_PARAGRAPH -> {
+                val view = LayoutInflater.from(context)
+                    .inflate(R.layout.article_detail_paragraph_item, parent, false)
+                ParagraphViewHolder(view)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
+        val element = adapterDataList[position]
+        when (holder) {
+            is ParagraphViewHolder -> holder.bind(element as ParagraphModel)
+            else -> throw IllegalArgumentException()
+        }
     }
 
     override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    override fun onBindViewHolder(holder: BaseViewHolder<*>, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    inner class ParagraphViewHolder(itemView: View) : BaseViewHolder<ParagraphModel>(itemView) {
 
+        private val textView = itemView.findViewById<TextView>(R.id.paragraph_content)
+
+        override fun bind(item: ParagraphModel) {
+            textView.text = item.content
+        }
+
+    }
 }
---
+++ app/src/main/java/com/damiengo/websiterss/ui/articledetail/model/ParagraphModel.kt
@@ -0,0 +1,7 @@
+package com.damiengo.websiterss.ui.articledetail.model
+
+class ParagraphModel(_content: String) : Model {
+
+    val content = _content
+
+}
