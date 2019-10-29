ed7e162352fc2bdbe9bd40a79448276f5a721bb3
damiengo <d@geon.fr>
2019-10-29 15:11:58 +0100

    Retrofit

--- app/src/main/java/com/damiengo/websiterss/article/JsonProviderStrategy.kt
+++ app/src/main/java/com/damiengo/websiterss/article/JsonProviderStrategy.kt
@@ -1,15 +1,50 @@
 package com.damiengo.websiterss.article
 
 import com.damiengo.websiterss.article.json.Api
+import com.damiengo.websiterss.article.json.Item
+import com.damiengo.websiterss.article.json.ItemList
+import com.damiengo.websiterss.article.json.Paragraph
+import com.damiengo.websiterss.ui.articledetail.model.Model
+import com.damiengo.websiterss.ui.articledetail.model.ParagraphModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
 
-class JsonProviderStrategy {
+class JsonProviderStrategy: Callback<ItemList> {
 
     @Inject
     lateinit var service: Api
 
     fun read(articleId: String) {
-        service.getItems(articleId)
+        val call: Call<ItemList> = service.getItems(articleId)
+
+        call.enqueue(this)
+
+
+    }
+
+    override fun onFailure(call: Call<ItemList>, t: Throwable) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onResponse(call: Call<ItemList>, response: Response<ItemList>) {
+        var models = mutableListOf<Model>()
+        if(response.isSuccessful) {
+            val itemList = response.body()
+            itemList?.let {list: ItemList ->
+                if(list.items.isNotEmpty()) {
+                    list.items.forEach {item: Item ->
+                        if(item.objet.paragraphs.isNotEmpty()) {
+                            item.objet.paragraphs.forEach {paragraph: Paragraph ->
+                                val model = ParagraphModel(paragraph.content)
+                                models.add(model)
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/damiengo/websiterss/article/json/Paragraph.kt
+++ app/src/main/java/com/damiengo/websiterss/article/json/Paragraph.kt
@@ -1,7 +1,25 @@
 package com.damiengo.websiterss.article.json
 
+import com.google.gson.annotations.SerializedName
+
 class Paragraph {
 
+    @SerializedName("__type")
+    lateinit var type: String
 
+    @SerializedName("content")
+    lateinit var content: String
+
+    @SerializedName("is_focus")
+    var isFocus: Boolean = false
+
+    @SerializedName("layout")
+    lateinit var layout: String
+
+    @SerializedName("number")
+    var number: Int = 0
+
+    @SerializedName("title")
+    lateinit var title: String
 
 }
