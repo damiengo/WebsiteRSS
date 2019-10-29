b5b16e980203abf964bc95dfa9674613a30478fd
damiengo <d@geon.fr>
2019-10-29 22:37:16 +0100

    Retrofit

--- app/src/main/java/com/damiengo/websiterss/article/json/Api.kt
+++ app/src/main/java/com/damiengo/websiterss/article/json/Api.kt
@@ -1,12 +1,11 @@
 package com.damiengo.websiterss.article.json
 
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface Api {
 
     @GET("/efr/news/{article_id}")
-    fun getItems(@Path("article_id") articleId: String): Call<ItemList>
+    suspend fun getItems(@Path("article_id") articleId: String): ItemList
 
 }
