95f289eefd65c84fa6289a04d605fd4154c4a208
damiengo <d@geon.fr>
2019-10-29 22:37:16 +0100

    Retrofit

--- app/src/main/java/com/damiengo/websiterss/article/JsonProviderStrategy.kt
+++ app/src/main/java/com/damiengo/websiterss/article/JsonProviderStrategy.kt
@@ -6,45 +6,31 @@ import com.damiengo.websiterss.article.json.ItemList
 import com.damiengo.websiterss.article.json.Paragraph
 import com.damiengo.websiterss.ui.articledetail.model.Model
 import com.damiengo.websiterss.ui.articledetail.model.ParagraphModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import javax.inject.Inject
 
-class JsonProviderStrategy: Callback<ItemList> {
+class JsonProviderStrategy : ProviderStrategy {
 
     @Inject
     lateinit var service: Api
 
-    fun read(articleId: String) {
-        val call: Call<ItemList> = service.getItems(articleId)
-
-        call.enqueue(this)
-
-
-    }
-
-    override fun onFailure(call: Call<ItemList>, t: Throwable) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onResponse(call: Call<ItemList>, response: Response<ItemList>) {
+    suspend fun read(articleId: String) {
         var models = mutableListOf<Model>()
-        if(response.isSuccessful) {
-            val itemList = response.body()
-            itemList?.let {list: ItemList ->
-                if(list.items.isNotEmpty()) {
-                    list.items.forEach {item: Item ->
-                        if(item.objet.paragraphs.isNotEmpty()) {
-                            item.objet.paragraphs.forEach {paragraph: Paragraph ->
-                                val model = ParagraphModel(paragraph.content)
-                                models.add(model)
-                            }
+
+        val itemList = service.getItems(articleId)
+
+        itemList?.let {list: ItemList ->
+            if(list.items.isNotEmpty()) {
+                list.items.forEach {item: Item ->
+                    if(item.objet.paragraphs.isNotEmpty()) {
+                        item.objet.paragraphs.forEach {paragraph: Paragraph ->
+                            val model = ParagraphModel(paragraph.content)
+                            models.add(model)
                         }
                     }
                 }
             }
         }
+
     }
 
 }
