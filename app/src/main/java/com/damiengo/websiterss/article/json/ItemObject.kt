00873d4492ee691363e35152189f1352862bb5dd
damiengo <d@geon.fr>
2019-11-21 22:35:21 +0100

    Infos model

--- app/src/main/java/com/damiengo/websiterss/article/json/ItemObject.kt
+++ app/src/main/java/com/damiengo/websiterss/article/json/ItemObject.kt
@@ -4,6 +4,7 @@ import com.damiengo.websiterss.ui.articledetail.model.Model
 import com.damiengo.websiterss.ui.articledetail.model.ModelFactory
 import com.damiengo.websiterss.util.DaggerDaggerComponent
 import com.google.gson.annotations.SerializedName
+import java.util.*
 import javax.inject.Inject
 
 class ItemObject {
@@ -17,6 +18,12 @@ class ItemObject {
     @SerializedName("long_title")
     lateinit var title: String
 
+    @SerializedName("date_update")
+    lateinit var dateUpdate: Date
+
+    @SerializedName("subhead")
+    lateinit var subhead: Subhead
+
     init {
         DaggerDaggerComponent.create().inject(this)
     }
@@ -26,6 +33,10 @@ class ItemObject {
 
         if(hasTitle()) {
             models.add(modelFactory.buildTitle(title))
+        }
+
+        if(hasSubhead()) {
+            models.add(modelFactory.buildInfoModel(dateUpdate, subhead.elements))
         }
 
         if(hasParagraphs()) {
@@ -39,6 +50,10 @@ class ItemObject {
 
     private fun hasTitle(): Boolean {
         return ::title.isInitialized
+    }
+
+    private fun hasSubhead(): Boolean {
+        return ::subhead.isInitialized
     }
 
     private fun hasParagraphs(): Boolean {
---
+++ app/src/main/java/com/damiengo/websiterss/ui/articledetail/model/InfoModel.kt
@@ -0,0 +1,10 @@
+package com.damiengo.websiterss.ui.articledetail.model
+
+import java.util.*
+
+class InfoModel(_pubDate: Date, _categories: String) : Model {
+
+    val pubDate: Date = _pubDate
+    val categories: String = _categories
+
+}
