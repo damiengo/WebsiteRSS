47a0b5d31d041cdffb20a699db532969e87d39f2
damiengo <d@geon.fr>
2019-12-04 22:19:42 +0100

    Dark theme

--- app/src/main/java/com/damiengo/websiterss/article/json/Element.kt
+++ app/src/main/java/com/damiengo/websiterss/article/json/Element.kt
@@ -7,4 +7,16 @@ class Element {
     @SerializedName("libelle")
     lateinit var libelle: String
 
+    fun getLibelleText(): String {
+        if(hasLibelle()) {
+            return libelle
+        }
+
+        return ""
+    }
+
+    private fun hasLibelle(): Boolean {
+        return ::libelle.isInitialized
+    }
+
 }
