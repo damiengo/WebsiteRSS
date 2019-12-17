af118c67175e2d412b19a596b460acaa3f247f36
damiengo <d@geon.fr>
2019-12-17 21:26:49 +0100

    Title dom parsing

--- app/src/main/java/com/damiengo/websiterss/article/DomProviderStrategy.kt
+++ app/src/main/java/com/damiengo/websiterss/article/DomProviderStrategy.kt
@@ -1,12 +1,12 @@
 package com.damiengo.websiterss.article
 
-import com.damiengo.websiterss.ui.articledetail.model.ChapoModel
-import com.damiengo.websiterss.ui.articledetail.model.Model
-import com.damiengo.websiterss.ui.articledetail.model.ParagraphModel
+import com.damiengo.websiterss.ui.articledetail.model.*
 import com.damiengo.websiterss.util.DaggerDaggerComponent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jsoup.nodes.Document
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 class DomProviderStrategy : ProviderStrategy {
@@ -29,10 +29,15 @@ class DomProviderStrategy : ProviderStrategy {
         }
 
         val models = mutableListOf<Model>()
+        models.add(TitleModel(getTitle()))
         models.add(ChapoModel(getChapo()))
         models.add(ParagraphModel(getDescription()))
 
         return models
+    }
+
+    private fun getTitle(): String {
+        return dom.select(".article__title").text()
     }
 
     private fun getChapo(): String {
