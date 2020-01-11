678a6802714a1ec5d57c6a1644daa08775b6baf1
damiengo <d@geon.fr>
2020-01-11 22:31:55 +0100

    New categories

--- app/src/main/java/com/damiengo/websiterss/category/ClassCategoryBuilder.kt
+++ app/src/main/java/com/damiengo/websiterss/category/ClassCategoryBuilder.kt
@@ -13,7 +13,12 @@ class ClassCategoriesBuilder @Inject constructor() : CategoriesBuilder  {
             R.id.nav_basket     to Category("Basket",     "http://www.lequipe.fr/rss/actu_rss_Basket.xml"),
             R.id.nav_tennis     to Category("Tennis",     "http://www.lequipe.fr/rss/actu_rss_Tennis.xml"),
             R.id.nav_rugby      to Category("Rugby",      "http://www.lequipe.fr/rss/actu_rss_Rugby.xml"),
-            R.id.nav_cyclisme   to Category("Cyclisme",   "http://www.lequipe.fr/rss/actu_rss_Cyclisme.xml")
+            R.id.nav_cyclisme   to Category("Cyclisme",   "http://www.lequipe.fr/rss/actu_rss_Cyclisme.xml"),
+            R.id.nav_automoto   to Category("Auto-Moto",  "http://www.lequipe.fr/rss/actu_rss_Auto-Moto.xml"),
+            R.id.nav_handball   to Category("Handball",   "http://www.lequipe.fr/rss/actu_rss_Hand.xml"),
+            R.id.nav_athle      to Category("Athlétisme", "http://www.lequipe.fr/rss/actu_rss_Athletisme.xml"),
+            R.id.nav_ski        to Category("Ski",        "http://www.lequipe.fr/rss/actu_rss_Ski.xml"),
+            R.id.nav_voile      to Category("Voile",      "http://www.lequipe.fr/rss/actu_rss_Voile.xml")
         )
     }
 
