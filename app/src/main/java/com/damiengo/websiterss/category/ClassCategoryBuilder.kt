49de89302df8a88ad7b2f88b87d7178d95d1db8b
damiengo <d@geon.fr>
2023-06-22 06:51:45 +0200

    Correction adresses RSS + màj versions

--- app/src/main/java/com/damiengo/websiterss/category/ClassCategoryBuilder.kt
+++ app/src/main/java/com/damiengo/websiterss/category/ClassCategoryBuilder.kt
@@ -7,18 +7,18 @@ class ClassCategoriesBuilder @Inject constructor() : CategoriesBuilder  {
 
     override fun build(): Map<Int, Category> {
         return mapOf(
-            R.id.nav_actu       to Category("Actualité",  "http://www.lequipe.fr/rss/actu_rss.xml"),
-            R.id.nav_foot       to Category("Football",   "http://www.lequipe.fr/rss/actu_rss_Football.xml"),
-            R.id.nav_transferts to Category("Transferts", "http://www.lequipe.fr/rss/actu_rss_Transferts.xml"),
-            R.id.nav_basket     to Category("Basket",     "http://www.lequipe.fr/rss/actu_rss_Basket.xml"),
-            R.id.nav_tennis     to Category("Tennis",     "http://www.lequipe.fr/rss/actu_rss_Tennis.xml"),
-            R.id.nav_rugby      to Category("Rugby",      "http://www.lequipe.fr/rss/actu_rss_Rugby.xml"),
-            R.id.nav_cyclisme   to Category("Cyclisme",   "http://www.lequipe.fr/rss/actu_rss_Cyclisme.xml"),
-            R.id.nav_automoto   to Category("Auto-Moto",  "http://www.lequipe.fr/rss/actu_rss_Auto-Moto.xml"),
-            R.id.nav_handball   to Category("Handball",   "http://www.lequipe.fr/rss/actu_rss_Hand.xml"),
-            R.id.nav_athle      to Category("Athlétisme", "http://www.lequipe.fr/rss/actu_rss_Athletisme.xml"),
-            R.id.nav_ski        to Category("Ski",        "http://www.lequipe.fr/rss/actu_rss_Ski.xml"),
-            R.id.nav_voile      to Category("Voile",      "http://www.lequipe.fr/rss/actu_rss_Voile.xml")
+            R.id.nav_actu       to Category("Actualité",  "https://dwh.lequipe.fr/api/edito/rss?path=/"),
+            R.id.nav_foot       to Category("Football",   "https://dwh.lequipe.fr/api/edito/rss?path=/Football/"),
+            R.id.nav_transferts to Category("Transferts", "https://dwh.lequipe.fr/api/edito/rss?path=/Football/Transferts-football/"),
+            R.id.nav_basket     to Category("Basket",     "https://dwh.lequipe.fr/api/edito/rss?path=/Basket/"),
+            R.id.nav_tennis     to Category("Tennis",     "https://dwh.lequipe.fr/api/edito/rss?path=/Tennis/"),
+            R.id.nav_rugby      to Category("Rugby",      "https://dwh.lequipe.fr/api/edito/rss?path=/Rugby/"),
+            R.id.nav_cyclisme   to Category("Cyclisme",   "https://dwh.lequipe.fr/api/edito/rss?path=/Cyclisme/"),
+            R.id.nav_automoto   to Category("Auto-Moto",  "https://dwh.lequipe.fr/api/edito/rss?path=/Auto-moto/"),
+            R.id.nav_handball   to Category("Handball",   "https://dwh.lequipe.fr/api/edito/rss?path=/Handball/"),
+            R.id.nav_athle      to Category("Athlétisme", "https://dwh.lequipe.fr/api/edito/rss?path=/Athletisme/"),
+            R.id.nav_ski        to Category("Ski",        "https://dwh.lequipe.fr/api/edito/rss?path=/Ski/"),
+            R.id.nav_voile      to Category("Voile",      "https://dwh.lequipe.fr/api/edito/rss?path=/Voile/")
         )
     }
 
--- app/src/main/java/com/damiengo/websiterss/ui/articledetail/ArticleDetailActivity.kt
+++ app/src/main/java/com/damiengo/websiterss/ui/articledetail/ArticleDetailActivity.kt
@@ -1,6 +1,5 @@
 package com.damiengo.websiterss.ui.articledetail
 
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.MenuItem
@@ -8,16 +7,16 @@ import android.view.View
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.damiengo.websiterss.R
 import com.damiengo.websiterss.article.ArticleDetailProvider
 import com.damiengo.websiterss.article.ProviderStrategy
 import com.damiengo.websiterss.comment.JsonProvider
+import com.damiengo.websiterss.databinding.ArticleDetailActivityBinding
+import com.damiengo.websiterss.di.DaggerComponent
 import com.damiengo.websiterss.di.DaggerDaggerComponent
 import com.damiengo.websiterss.ui.articledetail.model.Model
 import com.damiengo.websiterss.ui.articledetail.model.SiteLinkModel
 import com.damiengo.websiterss.util.GlideApp
 import com.damiengo.websiterss.util.ThemeUtil
-import kotlinx.android.synthetic.main.article_detail_activity.*
 import kotlinx.coroutines.*
 import javax.inject.Inject
 
@@ -37,19 +36,23 @@ class ArticleDetailActivity : AppCompatActivity() {
     private val viewModelJob = Job()
     private val scope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
+    private lateinit var binding: ArticleDetailActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         themeUtil = ThemeUtil(this)
         themeUtil.applyTheme()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.article_detail_activity)
+        binding = ArticleDetailActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         DaggerDaggerComponent.create().inject(this)
 
-        progress_bar.visibility = View.VISIBLE
+        binding.progressBar.visibility = View.VISIBLE
 
-        article_content.layoutManager = LinearLayoutManager(this)
+        binding.articleContent.layoutManager = LinearLayoutManager(this)
 
-        setSupportActionBar(article_toolbar)
+        setSupportActionBar(binding.articleToolbar)
         val actionbar: ActionBar? = supportActionBar
         actionbar?.apply {
             setDisplayHomeAsUpEnabled(true)
@@ -64,12 +67,12 @@ class ArticleDetailActivity : AppCompatActivity() {
             link = uri.scheme+"://"+uri.host+uri.path
         }
 
-        collapsing_toolbar.title = title
+        binding.collapsingToolbar.title = title
 
         GlideApp.with(this)
              .load(image)
              .centerCrop()
-             .into(article_image)
+             .into(binding.articleImage)
 
         scope.launch(Dispatchers.IO) {
             run breaker@{
@@ -79,14 +82,14 @@ class ArticleDetailActivity : AppCompatActivity() {
 
                     if (models.isNotEmpty()) {
                         withContext(Dispatchers.Main) {
-                            viewAdapter = ArticleDetailAdapter(this@ArticleDetailActivity, article_image, collapsing_toolbar)
+                            viewAdapter = ArticleDetailAdapter(this@ArticleDetailActivity, binding.articleImage, binding.collapsingToolbar)
 
                             models.forEach { model: Model ->
                                 viewAdapter.addModel(model)
                             }
 
-                            article_content.adapter = viewAdapter
-                            progress_bar.visibility = View.INVISIBLE
+                            binding.articleContent.adapter = viewAdapter
+                            binding.progressBar.visibility = View.INVISIBLE
                         }
                         viewAdapter.addModel(SiteLinkModel(link))
                         val commentModels = commentProvider.read(link)
