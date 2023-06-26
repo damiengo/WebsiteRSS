9e9ecce99ca76deb563f520a5a5b6060a4d319a7
damiengo <d@geon.fr>
2023-06-26 22:31:28 +0200

    Date format with days

--- app/src/main/java/com/damiengo/websiterss/article/ArticleUtil.kt
+++ app/src/main/java/com/damiengo/websiterss/article/ArticleUtil.kt
@@ -1,17 +1,22 @@
 package com.damiengo.websiterss.article
 
+import android.text.format.DateUtils
 import java.text.SimpleDateFormat
 import java.util.*
 import javax.inject.Inject
 
 class ArticleUtil @Inject constructor() {
 
+
     companion object{
 
-        const val titleCatMaxLength = 30
-        const val rssInputDateFormat = "EEE, dd MMM yyyy HH:mm:ss z"
-        const val outputValFormat = "HH:mm"
-        const val domaineName = "lequipe.fr"
+        const val TITLE_CAT_MAX_LEN = 30
+        const val RSS_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss z"
+        const val DATE_HOUR_FORMAT = "HH:mm"
+        const val DATE_DAY_HOUR_FORMAT = "dd/MM HH:mm"
+        const val DOMAIN_NAME = "lequipe.fr"
+        const val URL_SEPARATOR = "/"
+        const val CAT_SEPARATOR = " - "
 
     }
 
@@ -20,7 +25,7 @@ class ArticleUtil @Inject constructor() {
             val lastIdx = categoryLastIndex(it)
 
             if(lastIdx != -1) {
-                return it.substring(lastIdx+3)
+                return it.substring(lastIdx + CAT_SEPARATOR.length)
             }
 
             return it
@@ -34,7 +39,7 @@ class ArticleUtil @Inject constructor() {
             val lastIdx = categoryLastIndex(it)
 
             if(lastIdx != -1) {
-                return it.substring(0, lastIdx).split(" - ").toMutableList()
+                return it.substring(0, lastIdx).split(CAT_SEPARATOR).toMutableList()
             }
         }
 
@@ -46,29 +51,33 @@ class ArticleUtil @Inject constructor() {
             if(pubDate == "") {
                 return ""
             }
-            val inputFormat = SimpleDateFormat(rssInputDateFormat, Locale.ENGLISH)
+            val inputFormat = SimpleDateFormat(RSS_DATE_FORMAT, Locale.ENGLISH)
             inputFormat.timeZone = TimeZone.getTimeZone("Europe/Paris")
-            val outputFormat = this.getOutputFormat()
-            return outputFormat.format(inputFormat.parse(it)!!)
+            val inputDate = inputFormat.parse(it)!!
+            return genPubDateFromDate(inputDate)
         }
 
         return ""
     }
 
     fun genPubDateFromDate(pubDate: Date): String {
-        val outputFormat = this.getOutputFormat()
+        var outputFormat: SimpleDateFormat = this.getOutputFormat(DATE_DAY_HOUR_FORMAT)
+        if(DateUtils.isToday(pubDate.time)) {
+            outputFormat = this.getOutputFormat(DATE_HOUR_FORMAT)
+        }
+
         return outputFormat.format(pubDate)
     }
 
-    private fun getOutputFormat(): SimpleDateFormat {
-        val outputFormat = SimpleDateFormat(outputValFormat, Locale.ENGLISH)
+    private fun getOutputFormat(format: String): SimpleDateFormat {
+        val outputFormat = SimpleDateFormat(format, Locale.ENGLISH)
         outputFormat.timeZone = TimeZone.getTimeZone("Europe/Paris")
 
         return outputFormat
     }
 
     fun getArticleIdFromUrl(url: String): String {
-        val splitted = url.split("/")
+        val splitted = url.split(URL_SEPARATOR)
 
         if(splitted.isNotEmpty()) {
             val urlEnd = splitted.last()
@@ -84,23 +93,23 @@ class ArticleUtil @Inject constructor() {
     }
 
     fun getArticleCategorySlugFromUrl(url: String): String {
-        val domainIndex = url.indexOf(domaineName)
-        val lastSlashIndex = url.lastIndexOf("/")
+        val domainIndex = url.indexOf(DOMAIN_NAME)
+        val lastSlashIndex = url.lastIndexOf(URL_SEPARATOR)
 
         if(domainIndex != -1) {
-            return url.substring(domainIndex+domaineName.length+1, lastSlashIndex)
+            return url.substring(domainIndex+DOMAIN_NAME.length+1, lastSlashIndex)
         }
 
         return ""
     }
 
     private fun categoryLastIndex(title: String): Int {
-        var titleCatLength = titleCatMaxLength
-        if(title.length < titleCatMaxLength) {
+        var titleCatLength = TITLE_CAT_MAX_LEN
+        if(title.length < TITLE_CAT_MAX_LEN) {
             titleCatLength = title.length
         }
 
-        return title.substring(0, titleCatLength).lastIndexOf(" - ")
+        return title.substring(0, titleCatLength).lastIndexOf(CAT_SEPARATOR)
     }
 
     fun removeLinksFromText(text: String): String {
